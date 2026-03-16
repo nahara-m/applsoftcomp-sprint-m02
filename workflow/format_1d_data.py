29083c3ce7e517ee6db7cec96397cfc6654597b0
Narrara | Nahara <nmelomoura@binghamton.edu>
2026-03-15 23:53:57 -0400

    fix code to ensure preprocessed is folder is created too (note rounding was not really needed here for the viz to have been created)

--- workflow/format_1d_data.py
+++ workflow/format_1d_data.py
@@ -1,8 +1,11 @@
 #format 1d dataset for data viz
 import pandas as pd
+import os
 
 df_1d = pd.read_csv('./data/1d-data.csv')
 df_1d = df_1d.round(2)
 
 #save preprocessed data to new csv file
+#add new folder preprocessed to data folder and save the formatted data there   
+os.makedirs('./data/preprocessed', exist_ok=True)
 df_1d.to_csv('./data/preprocessed/1d-data-formatted.csv', index=False)
